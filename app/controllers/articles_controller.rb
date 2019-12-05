165433bd813e4d67968b69327ef7fa7805720be2
guru-schnecke <eiweala@live.com>
2019-12-05 14:23:19 +0300

    check point

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,26 @@
 class ArticlesController < ApplicationController
 
  def index
-  
+  @article = Article.all
  end
 
+ def new
+
+ end
+
+ def create
+  # render plain: params[:article].inspect
+  @article = Article.new(article_params)
+ 
+  @article.save
+  redirect_to @article
+ end
+
+
+
+ private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+ 
 end
